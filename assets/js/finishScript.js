50b0b04b59210c424ba0205eec871fdc929dde84
RiccardoPri <118051660+RiccardoPri@users.noreply.github.com>
2023-01-13 19:05:03 +0100

    V1.0
    Add try of geolocation with locationIQ - works
    Test json files added cause of testing

--- assets/js/finishScript.js
+++ assets/js/finishScript.js
@@ -125,4 +125,61 @@ $(document).ready(function () {
           }
           
     });
+
+/************ geolocate City Date if not input user city ***********/ 
+
+function getCoordintes() {
+    var options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+    };
+  
+    function success(pos) {
+        var crd = pos.coords;
+        var lat = crd.latitude.toString();
+        var lng = crd.longitude.toString();
+        var coordinates = [lat, lng];
+        console.log(`Latitude: ${lat}, Longitude: ${lng}`);
+        getCity(coordinates);
+        return;
+  
+    }
+  
+    function error(err) {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+  
+    navigator.geolocation.getCurrentPosition(success, error, options);
+}
+
+function getCity(coordinates) {
+    var xhr = new XMLHttpRequest();
+    var lat = coordinates[0];
+    var lng = coordinates[1];
+    
+    console.log(lat);
+    console.log(lng);
+
+    // Paste your LocationIQ token below.
+    xhr.open('GET', "https://us1.locationiq.com/v1/reverse.php?key=pk.ed945b62c3304a249d198ef1ed8786aa&lat=" + lat + "&lon=" + lng + "&format=json", true);
+    xhr.send();
+    xhr.onreadystatechange = processRequest;
+    xhr.addEventListener("readystatechange", processRequest, false);
+  
+    function processRequest(e) {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var response = JSON.parse(xhr.responseText);
+            var responseString = response.display_name;
+            const adressArr = responseString.split(', ');
+            let city = adressArr[3];
+            console.log(city);
+            return;
+        }
+    }
+}
+  
+getCoordintes();
+
+    
 });
